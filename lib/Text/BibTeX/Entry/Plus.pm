c2c6b094edf714924778aef1c02ccfb100517611
Ryo Nakamura <nakamura@zebulun.net>
2019-08-06 18:44:06 +0900

    fix a mapper for japanese month

--- lib/Text/BibTeX/Entry/Plus.pm
+++ lib/Text/BibTeX/Entry/Plus.pm
@@ -195,8 +195,8 @@ sub export_date {
 
 sub export_japanese_date {
     my %JAPANESE_MONTH = (
-        'Jan' => 1, 'Feb' => 2,  'Mar'  => 3, 'Apr'  => 4,
-        'May' => 5, 'Jun' => 6,  'Jul'  => 7, 'Apr'  => 8,
+        'Jan' => 1, 'Feb' => 2,  'Mar' => 3,  'Apr' => 4,
+        'May' => 5, 'Jun' => 6,  'Jul' => 7,  'Aug' => 8,
         'Sep' => 9, 'Oct' => 10, 'Nov' => 11, 'Dec' => 12,
     );
     my $self = shift;
